5b6b27c0b106584ccf9c149dfee94982b7f0d35f
Howard Henry <howardahenry@gmail.com>
2022-02-18 10:18:24 -0500

    feat: add not found route handler to express

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const responseTime = require('response-time');
 const log = require('./utils/log');
 const requestLogger = require('./views/middleware/request-logger');
 const handleMemberValidations = require('./views/routes/handlers/member-validations');
+const handleNotFound = require('./views/routes/handlers/not-found');
 
 const app = express();
 
@@ -10,5 +11,8 @@ app.use(responseTime());
 app.use(requestLogger());
 
 app.use('/member-validations', handleMemberValidations);
+
+// Fallback 404 route
+app.use('/*', handleNotFound);
 
 app.listen(3000, () => log.info(`Server started on port 3000`));
